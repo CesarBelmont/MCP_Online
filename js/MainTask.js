44dec06f96b1d4e1aa73d022b720b50f9c082574
Cesar Rea <carr_675@hotmail.com>
2020-08-29 20:32:24 -0500

    Acomodo de archivos y guardado de variables

--- js/MainTask.js
+++ js/MainTask.js
@@ -1,16 +1,13 @@
 
 function hideAll() { //Hide all elements (Buttons and the NoGoIndicator)
     document.getElementById("readInstruction").style.display = "none";
-    document.getElementById("readConsent").style.display = "none";
-    document.getElementById("startDemographics").style.display = "none";
+    document.getElementById("userData").style.display = "none";
     document.getElementById("startPractice").style.display = "none";
     document.getElementById("startPhase1").style.display = "none";
     document.getElementById("startPhase2").style.display = "none";
-    document.getElementById("startSurvey").style.display = "none";
     document.getElementById("ClosePage").style.display = "none";
-    document.getElementById("NoGoIndicator").style.display = "none";
-    document.getElementById("mturk_form").style.display = "none";
     document.getElementById("initialize").style.display = "none";
+
 }
 
 function updateMenu(expStage) { //Depending on the stage it will display a diferent button
@@ -19,43 +16,31 @@ function updateMenu(expStage) { //Depending on the stage it will display a difer
     switch (numStage) {     
         case 0:
             hideAll();
-            document.getElementById("initialize").textContent = "Please read the informed consent carefully before agreeing to participate in this study.";
+            document.getElementById("initialize").textContent = "We need some data first, plase fill the info required";
             document.getElementById("initialize").style.display = "inline";
-            document.getElementById("readConsent").style.display = "inline";
+            document.getElementById("userData").style.display = "inline";
             break;
         case 1:
-            hideAll();
-            if (sessionStorage.getItem("Consent") === "true") {
-                document.getElementById("initialize").textContent = "To continue, please fill out a demographic survey.";
-                document.getElementById("initialize").style.display = "inline";
-                document.getElementById("startDemographics").style.display = "inline";
-            } else {
-                document.getElementById("NoGoIndicator").style.display = "inline";
-            }
-            break;
-        case 2:
             hideAll();
             document.getElementById("initialize").textContent = "Let's start with some sample practice trials!";
             document.getElementById("initialize").style.display = "inline";
             document.getElementById("startPractice").style.display = "inline";
             break;
-        case 3:
+        case 2:
             hideAll();
             document.getElementById("initialize").textContent = "Ready to start the experiment?";
             document.getElementById("initialize").style.display = "inline";
             document.getElementById("startPhase1").style.display = "inline";
             break;
-        case 4:
+        case 3:
             hideAll();
             document.getElementById("initialize").textContent = "Phase II: Ready?";
             document.getElementById("initialize").style.display = "inline";
             document.getElementById("startPhase2").style.display = "inline";
             break;
-        case 5:
+        case 4:
             hideAll();
-            document.getElementById("initialize").textContent = "Thank you for your participation. Press the button to submit your score";                  
-            document.getElementById("submitButton").style.display = "inline";
-            document.getElementById("mturk_form").style.display = "inline";
+            document.getElementById("initialize").textContent = "Thank you for your participation. Press the button to submit your score";
             document.getElementById("initialize").style.display = "inline";
             break;
         default:
@@ -68,6 +53,5 @@ function updateMenu(expStage) { //Depending on the stage it will display a difer
 
 document.addEventListener('DOMContentLoaded', function () {   
     hideAll();
-   
     updateMenu(sessionStorage.getItem("Stage"));
 });
