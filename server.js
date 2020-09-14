bfcc3f8553f303898e17a05aa89159cc80072be3
Cesar Rea <carr_675@hotmail.com>
2020-09-14 18:30:29 -0500

    Guardado de datos en un archivo JSON

--- server.js
+++ server.js
@@ -36,123 +36,98 @@ app.get('/task/PhaseIII.html', (req, res) => {
 });
 
 app.post('/task/End.html', (req, res) => {
-    res.sendFile('./task/End.html', { root: __dirname });
 
+    res.sendFile('./task/End.html', { root: __dirname });
     var fs = require('fs');
     var id = req.body.id;
     var finalScore = req.body.finalscore;
-    var cueprobID= req.body.cueprobID;
-    var action1= req.body.action1;
-    var decision1=req.body.decision1;
-    var responseT1= req.body.responseT1;
-    var cuesL= req.body.cuesL;
-    var cuesR= req.body.cuesR;
-    var seqID= req.body.seqID;
-    var action2=req.body.action2;
-    var decision2=req.body.decision2;
-    var responseT2=req.body.responseT2;
-    var cuetestL=req.body.cuetestL;
-    var cuetestR=req.body.cuetestR;
-    var action3=req.body.action3;
-    var decision3=req.body.decision3;
-    var responseT3=req.body.responseT3;
-    var cuetestL2=req.body.cuetestL2;
-    var cuetestR2 = req.body.cuetestR2;
-    fs.appendFile('data.txt', id + '\n\r', function (err) {
+    var cueprobID = req.body.cueprobID;
+    var action1 = (req.body.action1).split(",");
+    var decision1 = (req.body.decision1).split(",");
+    var responseT1 = (req.body.responseT1).split(",");
+    var cuesL = (req.body.cuesL).split(",");
+    var cuesR = (req.body.cuesR).split(",");
+    var seqID = req.body.seqID;
+    var action2 = (req.body.action2).split(",");
+    var decision2 = (req.body.decision2).split(",");
+    var responseT2 = (req.body.responseT2).split(",");
+    var cuetestL = (req.body.cuetestL).split(",");
+    var cuetestR = (req.body.cuetestR).split(",");
+    var action3 = (req.body.action3).split(",");
+    var decision3 = (req.body.decision3).split(",");
+    var responseT3 = (req.body.responseT3).split(",");
+    var cuetestL2 = (req.body.cuetestL2).split(",");
+    var cuetestR2 = (req.body.cuetestR2).split(",");
+    var trial = (req.body.Trial).split(",");
+    var trial2 = (req.body.Trial2).split(",");
+    var trial3 = (req.body.Trial3).split(",");
+    var datap1 = [];
+    var datap2 = [];
+    var datap3 = [];
+    for (i = 0; i < action1.length; i++) {
+        datap1[i] = {
+            "trial": trial[i],
+            "id": id,
+            "finalScore": finalScore,
+            "cueprobID": cueprobID,
+            "seqID": seqID,
+            "action": action1[i],
+            "decision": decision1[i],
+            "responseTime": responseT1[i],
+            "CuesL": cuesL[i],
+            "CuesR": cuesR[i],
+            "Phase": 1
+        }
+    }
+    for (i = 0; i < action2.length; i++) {
+        datap2[i] = {
+            "trial": trial2[i],
+            "id": id,
+            "finalScore": finalScore,
+            "cueprobID": cueprobID,
+            "seqID": seqID,
+            "action": action2[i],
+            "decision": decision2[i],
+            "responseTime": responseT2[i],
+            "CuesL": cuetestL[i],
+            "CuesR": cuetestR[i],
+            "Phase": 2
+        };
+    }
+    for (i = 0; i < action3.length; i++) {
+        datap3[i] = {
+            "trial": trial3[i],
+            "id": id,
+            "finalScore": finalScore,
+            "cueprobID": cueprobID,
+            "seqID": seqID,
+            "action": action3[i],
+            "decision": decision3[i],
+            "responseTime": responseT3[i],
+            "CuesL": cuetestL2[i],
+            "CuesR": cuetestR2[i],
+            "Phase": 3
+        };
+    }
+    if (!fs.existsSync('data.json')) {
+        let file = [];
+        fs.writeFile('data.json', JSON.stringify(file), function (err) {
+            if (err) throw err;
+        });
+    }
+    fs.readFile('data.json', function (err, data) {
         if (err) {
             console.log(err);
-        };
+        }
+        
+        var json = JSON.parse(data);       
+        json.push(datap1);
+        json.push(datap2);
+        json.push(datap3);
+        fs.writeFileSync('data.json', JSON.stringify(json, null, 2), function (err) {
+            if (err) throw err;
+        });
     });
-    fs.appendFile('data.txt', finalScore + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', cueprobID + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', action1 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', decision1 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', responseT1 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', cuesL + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', cuesR + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', seqID + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', action2 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', decision2 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', responseT2 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', cuetestL + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', cuetestR + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', action3 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', decision3 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', responseT3 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', cuetestL2 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    fs.appendFile('data.txt', cuetestR2 + '\n\r', function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });   
 });
 
 app.listen(port, () => console.log(`Ready in port ${port}!`));
